b892f1e2d82c7e08f3a2cf66184d0c9c4674a002
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-12 02:09:02 +0530

    Displayed heads or Tails as Winner

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,1 +1,11 @@
+#!/bin/bash -x
 echo Welcome to Flip Coin Simulation. This program displays the winner Heads or Tails
+
+coincheck=$((RANDOM%2))
+
+if [ $coincheck -eq 1 ]
+then
+	echo Heads
+else
+	echo Tails
+fi
